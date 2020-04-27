29c895faa767c94cbd8e8fd78bf81b9b4f7c3e1d
Jordi C <jordicea@gmail.com>
2020-04-27 18:54:08 +0200

    title

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,9 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: 'Jordi Cea, software engineer',
+    }
   },
   {
     path: '/about',
